331b3beacb6a04df358a15e2338fe7ce96e3c1d8
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-05-10 16:05:37 +0300

    Updated the code to work correctly

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,49 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+from django.http import Http404
+
+
+def get_entered(visit):
+    entered_at = visit.entered_at
+    entered = localtime(value=entered_at)
+    return entered
+
+
+def get_duration(visit):
+    entered_at = visit.entered_at
+    entered = localtime(value=entered_at)
+    leaved_at = visit.leaved_at
+    leaved = localtime(value=leaved_at)
+    delta = leaved - entered
+    duration = delta.seconds // 60
+    return duration
+
+
+def is_visit_long(visit, minutes=10):
+    if get_duration(visit) > minutes:
+        return False
+    else:
+        return True
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
-    context = {
-        'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
-    }
-    return render(request, 'passcard_info.html', context)
+    try:
+        passcard = Passcard.objects.get(passcode=passcode)
+        visits = Visit.objects.filter(passcard=passcard)
+        this_passcard_visits = []
+        for visit in visits:
+            this_passcard_visits.append({
+                'entered_at': f'{get_entered(visit)}',
+                'duration': f'{get_duration(visit)}',
+                'is_strange': f'{is_visit_long(visit, minutes=10)}',
+            }
+            )
+        context = {
+            'passcard': passcard,
+            'this_passcard_visits': this_passcard_visits
+        }
+        return render(request, 'passcard_info.html', context)
+    except Passcard.DoesNotExist:
+        raise Http404("No passcard matches the given query.")
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,46 +4,37 @@ from django.utils.timezone import localtime
 import time
 
 
-def get_duration():
-    visits = Visit.objects.filter(leaved_at=None)
-    durations = []
-    for visit in visits:
-        entered = visit.entered_at
-        moscow_time = localtime(value=entered)
-        now = localtime()
-        delta = now - moscow_time
-        seconds = delta.seconds
-        duration = time.strftime('%H:%M:%S', time.gmtime(seconds))
-        durations.append(duration)
-    return ' '.join([str(x) for x in durations])
+def get_duration(visit):
+    entered = visit.entered_at
+    moscow_time = localtime(value=entered)
+    now = localtime()
+    delta = now - moscow_time
+    seconds = delta.seconds
+    duration = time.strftime('%H:%M:%S', time.gmtime(seconds))
+    return duration
 
 
-def get_moscow_time():
-    visits = Visit.objects.filter(leaved_at=None)
-    moscow_times = []
-    for visit in visits:
-        entered = visit.entered_at
-        moscow_time = localtime(value=entered)
-        moscow_times.append(moscow_time)
-    return ' '.join([str(x) for x in moscow_times])
+def get_moscow_time(visit):
+    entered = visit.entered_at
+    moscow_time = localtime(value=entered)
+    return moscow_time
 
 
-def get_name():
-    visits = Visit.objects.filter(leaved_at=None)
-    names = []
-    for visit in visits:
-        name = visit.passcard.owner_name
-        names.append(name)
-    return ' '.join([str(x) for x in names])
+def get_name(visit):
+    name = visit.passcard.owner_name
+    return name
 
 
 def storage_information_view(request):
-    title = ['who_entered', 'entered_at', 'duration']
-    visit = [f'{get_name()}', f'{get_moscow_time()}', f'{get_duration()}']
-    list_visits = dict(zip(title, visit))
-    non_closed_visits = [
-        list_visits
-    ]
+    visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in visits:
+        non_closed_visits.append({
+            'who_entered': f'{get_name(visit)}',
+            'entered_at': f'{get_moscow_time(visit)}',
+            'duration': f'{get_duration(visit)}',
+        }
+        )
     context = {
         'non_closed_visits': non_closed_visits,
     }
