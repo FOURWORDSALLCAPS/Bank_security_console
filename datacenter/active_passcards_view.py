f3b664d524c67f1db6720c4211ccc6a6ce67d654
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-05-11 13:14:44 +0300

    Cleaned up the code from unnecessary comments

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def active_passcards_view(request):
     active_passcards = Passcard.objects.filter(is_active=True)
     context = {
-        'active_passcards': active_passcards,  # люди с активными пропусками
+        'active_passcards': active_passcards,
     }
     return render(request, 'active_passcards.html', context)
 
--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
 
-from datacenter.models import Visit  # noqa: E402
+from datacenter.models import Visit
 
 if __name__ == '__main__':
-    print('Количество пропусков:', Visit.objects.all())  # noqa: T001
+    print('Количество пропусков:', Visit.objects.all())
