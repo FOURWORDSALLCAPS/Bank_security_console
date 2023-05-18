f61f2b88f81613aab738ca122d038316d4c9e142
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-05-18 19:38:29 +0300

    Fixed allowed hosts setting

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["*"])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
